6fb113a53c35935348cfdccfecb3f52f43a98457
Sergio Gonzalez <bigmonachus@gmail.com>
2015-03-26 22:35:05 -0600

    fix slurp

--- serg_io.h
+++ serg_io.h
@@ -8,31 +8,34 @@
 
 #pragma once
 
-static const size_t bytes_in_fd(FILE* fd)
+
+static const int bytes_in_fd(FILE* fd)
 {
     fpos_t fd_pos;
     fgetpos(fd, &fd_pos);
     fseek(fd, 0, SEEK_END);
-    size_t len = (size_t)ftell(fd);
+    int len = ftell(fd);
     fsetpos(fd, &fd_pos);
     return len;
 }
 
-static const char* slurp_file(const char* path, size_t *out_size)
+static const char* slurp_file(const char* path, int *out_size)
 {
     FILE* fd = fopen(path, "r");
-    assert(fd);
     if (!fd)
     {
         fprintf(stderr, "ERROR: couldn't slurp %s\n", path);
+        assert(fd);
         return NULL;
     }
-    const size_t len = bytes_in_fd(fd);
-    const char* contents = malloc(len + 1);
-    const size_t read = fread((void*)contents, 1, len, fd);
+    int len = bytes_in_fd(fd);
+    char* contents = malloc(len + 1);
+    // TODO: Truncating from size_t to int... Technically a bug but will probably never happen
+    const int read = (int)fread((void*)contents, 1, (size_t)len, fd);
     // TODO: Do something about read != len?
     fclose(fd);
-    *out_size = read;
+    *out_size = read + 1;
+    contents[read] = '\0';
     return contents;
 }
 
