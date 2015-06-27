411df263a2f1d7ad5e0b8238c4b2c76c6ca7eb8e
Sergio Gonzalez <bigmonachus@gmail.com>
2015-06-27 17:10:27 -0500

    small types

--- defaults.h
+++ defaults.h
@@ -8,16 +8,23 @@ extern "C"
 {
 #endif
 
-// Rename types for convenience
-typedef int8_t      int8;
-typedef uint8_t     uint8;
-typedef int16_t     int16;
-typedef uint16_t    uint16;
-typedef int32_t     int32;
-typedef uint32_t    uint32;
-typedef int64_t     int64;
-typedef uint64_t    uint64;
-typedef int32_t     bool32;
+typedef int8_t	    i8;
+typedef int16_t	    i16;
+typedef int32_t     i32;
+typedef int64_t     i64;
+
+typedef uint8_t	    u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t    u64;
+
+typedef float	    f32;
+
+#ifndef b32
+#define		    b32 int
+#else
+#error b32 already defined
+#endif
 
 #ifndef true
 #define true 1
