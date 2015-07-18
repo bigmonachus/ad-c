7965860b55198a45eb5c91f605c41086ebd80648
Sergio Gonzalez <bigmonachus@gmail.com>
2015-07-18 16:34:11 -0500

    File to undef macros

---
+++ undef_defaults.h
@@ -0,0 +1,27 @@
+// undef_defaults.h
+// (c) Copyright 2015 Sergio Gonzalez
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#undef stack_count(a)
+#undef false
+#undef true
+#undef f32
+#undef u64
+#undef u32
+#undef u16
+#undef u8
+#undef i64
+#undef i32
+#undef i16
+#undef i8
+
+
+#ifdef __cplusplus
+}
+#endif
